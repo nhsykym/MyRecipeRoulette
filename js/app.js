224a57addd57997c48160d736a023a391e038286
Naohisa Yokoyama <nhsykym@gmail.com>
2020-02-18 22:08:58 +0900

    Create draft version of app(array based functionality)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,61 @@
+const AppHeader = () => {
+  return (
+    <div className="headerWrapper">
+      <h1>レシピルーレット</h1>
+      <p>ランダムにレシピを表示するよ</p>
+    </div>
+  );
+}
 
+const Result = (props) => {
+  return (
+    <div className="result">
+      {props.item==='' ? '' :<h2>今日は{props.item}を作りましょう</h2>}
+    </div>
+
+  );
+}
+
+const AppBody = (props) => {
+  return (
+    <div className="bodyWrapper">
+      <div className="btn" onClick={props.onClick}>?</div>
+      <Result item={props.item}/>
+    </div>
+  );
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: ["カレー", "にくじゃが", "チャーハン", "ハヤシライス", "ラーメン"],
+      item: '',
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const i = Math.floor(Math.random() * this.state.recipes.length);
+    const item = this.state.recipes[i];
+    this.setState({
+      item: item,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <AppHeader />
+        <AppBody item={this.state.item} onClick={this.handleClick}/>
+      </div>
+    );
+  }  
+}
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
