3a5bcdc247efa87bf05922b058957878c9abea5d
Naohisa Yokoyama <nhsykym@gmail.com>
2020-02-20 23:37:56 +0900

    Update api.php and delete composer.json

--- api.php
+++ api.php
@@ -1,25 +1,16 @@
 <?php
-
-  $url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1072861491823725264&categoryType=medium';
+  $url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1072861491823725264&categoryType=large';
   $json = file_get_contents($url);
   $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-
   $arr = json_decode($json,true);
-
-  echo $arr['result'];
-
+  
   if ($arr === NULL) {
-    return;//〜データがない時の処理〜
+    return;
   }else{
-    //〜存在しているときの処理〜
+    foreach ($arr['result'][large] as $category) {
+      echo $category['categoryName']."<br>";
+    }
   }
-
-
-
-
-
-
-
 
 ?>
 
